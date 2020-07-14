e04b8a2991b2324279d0094cd6b71706ac6e766e
Andrey O <ozzy@triasoft.com>
2020-07-14 23:13:10 +0300

    Add simople fixture tu access to ES

---
+++ src/MyLab.Elastic.Test/EsFixture.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MyLab.Elastic.Test
+{
+    /// <summary>
+    /// Provides manager to work with remote ES instance
+    /// </summary>
+    public class EsFixture : IAsyncLifetime
+    {
+        private readonly IConnectionPool _connection;
+        private readonly ElasticClient _client;
+
+        public IEsManager Manager { get; private set; }
+
+        /// <summary>
+        /// Test output. Set to get logs.
+        /// </summary>
+        public ITestOutputHelper Output { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="EsIndexFixture{TDoc}"/>
+        /// </summary>
+        public EsFixture()
+            :this(new EnvironmentConnectionProvider())
+        {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="EsIndexFixture{TDoc}"/>
+        /// </summary>
+        protected EsFixture(IConnectionProvider connectionProvider)
+        {
+            _connection = connectionProvider.Provide();
+            
+            var settings = new ConnectionSettings(_connection);
+            settings.DisableDirectStreaming();
+            settings.OnRequestCompleted(details =>
+            {
+                if (Output != null)
+                    TestEsLogger.Log(Output, details);
+            });
+
+            _client = new ElasticClient(settings);
+        }
+
+        public Task InitializeAsync()
+        {
+            Manager = new TestEsManager(_client);
+
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            _connection?.Dispose();
+
+            return Task.CompletedTask;
+        }
+    }
+}
