4f7f5a4f76ccdd93f2af9f9fe093df174f23cb69
Andrey O <ozzy.ext@gmail.com>
2020-12-08 00:41:16 +0300

    ResponseException when test index created and server return error

--- src/MyLab.Elastic.Test/TmpIndexLife.cs
+++ src/MyLab.Elastic.Test/TmpIndexLife.cs
@@ -27,7 +27,7 @@ namespace MyLab.Elastic.Test
                 resultIndexName, cd => cd.Map<TDoc>(md => md.AutoMap()));
 
             if (!res.ShardsAcknowledged)
-                throw new InvalidOperationException("Could not create index");
+                throw new ResponseException<CreateIndexResponse>("Could not create index", res);
 
             return new TmpIndexLife<TDoc>(client, resultIndexName)
             {
