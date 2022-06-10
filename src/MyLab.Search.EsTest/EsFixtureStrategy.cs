360f913dff748553695c168816f2c4ac5ead608a
Andrey O <ozzy.ext@gmail.com>
2022-06-10 13:41:11 +0300

    Comments and version up

--- src/MyLab.Search.EsTest/EsFixtureStrategy.cs
+++ src/MyLab.Search.EsTest/EsFixtureStrategy.cs
@@ -3,10 +3,19 @@ using Nest;
 
 namespace MyLab.Search.EsTest
 {
+    /// <summary>
+    /// Tunes <see cref="EsFixture{TStrategy}"/>
+    /// </summary>
     public abstract class EsFixtureStrategy
     {
+        /// <summary>
+        /// Provides Elasticsearch connection pool
+        /// </summary>
         public abstract IConnectionPool ProvideConnection();
 
+        /// <summary>
+        /// Creates settings for connection
+        /// </summary>
         public virtual ConnectionSettings CreateConnectionSettings(IConnectionPool connection)
         {
             return new ConnectionSettings(connection);
